8fb28f340c74c1dbac8f5cb57d4c5fe44be2043e
Nicholas Shulman <nicksh@uw.edu>
2022-04-11 11:36:45 -0700

    Unit tests for Skyline integration

--- bin/NickTest.py
+++ bin/NickTest.py
@@ -1,10 +1,5 @@
 import unittest
-
-import numpy as np
-
 import calculate_loq as calc_loq
-import numpy
-from sklearn import linear_model
 
 class TestCalculateLoq(unittest.TestCase):
     areas = [0.16751, 0.17056, 0.18132, 0.17482, 0.17060, 0.16879, 0.17469, 0.17645, 0.16372, 0.17310, 0.17941,
@@ -12,44 +7,39 @@ class TestCalculateLoq(unittest.TestCase):
              0.18260, 0.18245, 0.19410, 0.16476, 0.17444, 0.18214, 0.17844, 0.16537]
     concentrations = [0.005, 0.005, 0.005, 0.01, 0.01, 0.01, 0.03, 0.03, 0.03, 0.05, 0.05, 0.05, 0.07, 0.07, 0.07,
                       0.1, 0.1, 0.1, 0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.7, 0.7, 0.7, 1.0, 1.0, 1.0]
+    config = {'grid_size': 100, 'max_boot_iters': 100, 'min_boot_iters_for_measure': 10,
+              'min_same_loq_count_for_accept': 25, 'cv_threshold': 0.2, 'debug_plot': False,
+              'optimize_type': 1, 'minimum_num_transitions': 4}
 
     def test_fit_conc_vs_area(self):
         result = calc_loq.fit_conc_vs_area(self.concentrations, self.areas)
         slope = result['params'][0]
         intercept = result['params'][1]
         baseline_height = result['params'][2]
-        self.assertAlmostEqual(18.60910749, slope)
-        self.assertAlmostEqual(-3.00525332, intercept)
-        self.assertAlmostEqual(0.17916667, baseline_height)
-        self.assertAlmostEqual(94.99307214782094, result['error'])
-        self.assertAlmostEqual(0.3079829089774662, result['baseline_std'])
+        self.assertAlmostEqual(0.025854009062249942, slope)
+        self.assertAlmostEqual(0.16317916521766687, intercept)
+        self.assertAlmostEqual(0.172315, baseline_height)
+        self.assertAlmostEqual(0.057632885215053185, result['error'])
 
     def test_bilinear_fit(self):
         unique_concentrations, mean_areas = calc_loq.get_unique_conc_and_mean_areas(self.concentrations, self.areas)
         result = calc_loq.bilinear_fit(0.03, unique_concentrations, mean_areas)
-        self.assertAlmostEqual(0.01500812, result['params'][0])  # slope
-        self.assertAlmostEqual(0.16795589, result['params'][1])  # intercept
+        # [0.00239897 0.16965172 0.17205111]
+        self.assertAlmostEqual(0.00239897, result['params'][0])  # slope
+        self.assertAlmostEqual(0.16965172, result['params'][1])  # intercept
         self.assertAlmostEqual(0.17205111, result['params'][2])  # baseline
         self.assertAlmostEqual(0.07240250761116633, result['error'])
         self.assertAlmostEqual(0.0007680004501027473, result['baseline_std'])
 
-    def test_polynomial_fit(self):
-        x = [0.05, 0.07, 0.1, 0.3, 0.5, 0.7, 1.0]
-        y = [0.17310666666666666, 0.1674433333333333, 0.15965666666666667, 0.18022333333333332, 0.18300000000000002,
-             0.17776666666666666, 0.17531666666666668]
-        weights = [20.0, 14.285714285714285, 10.0, 3.3333333333333335, 2.0, 1.4285714285714286, 1.0]
-        result1 = numpy.polyfit(x, y, 1, w=weights)
-        result2 = numpy.polynomial.polynomial.Polynomial.fit(x, y, 1, w=weights)
-        regression = linear_model.LinearRegression()
-        x_values = [[el] for el in x]
-        result3 = regression.fit(x_values, y, weights)
-        self.assertAlmostEqual(0.16795589141065997, result2[0])
+    def test_compute_quantitative_limits(self):
+        concentrations = [0.005, 0.005, 0.005, 0.01, 0.01, 0.01, 0.03, 0.03, 0.03, 0.05, 0.05, 0.05, 0.07, 0.07, 0.07,
+                          0.1, 0.1, 0.1, 0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.7, 0.7, 0.7, 1.0, 1.0, 1.0]
 
-    def test_linear_regression(self):
-        x = [0.05, 0.07, 0.1, 0.3, 0.5, 0.7, 1.0]
-        y = [0.17310666666666666, 0.1674433333333333, 0.15965666666666667, 0.18022333333333332, 0.18300000000000002,
-         0.17776666666666666, 0.17531666666666668]
-        weights = [20.0, 14.285714285714285, 10.0, 3.3333333333333335, 2.0, 1.4285714285714286, 1.0]
-        result = calc_loq.linear_regression(x, y, w=weights)
-        result2 = np.polyfit(x, y, 1, w=weights)
-        self.assertEqual(len(result), len(result2))
+        areas = [0.14685, 0.1646, 0.21232, 0.15662, 0.16293, 0.13719, 0.17409, 0.16193, 0.13625, 0.16065, 0.16249, 0.16409, 0.14434, 0.17133, 0.16018, 0.16517, 0.16808, 0.14533, 0.16407, 0.1659, 0.16019, 0.17952, 0.16168, 0.16217, 0.19297, 0.14971, 0.16969, 0.17356, 0.16252, 0.15779]
+
+        result = calc_loq.compute_quantitative_limits(concentrations, areas, self.config)
+        self.assertAlmostEqual(0.8052500937105422, result[calc_loq.optimize_type.LOD])
+        self.assertAlmostEqual(1, result[calc_loq.optimize_type.LOQ])
+
+    def test_compute_bootstrapped_loq(self):
+        result = calc_loq.compute_quantitative_limits(self.concentrations, self.areas, self.config)
